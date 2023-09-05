9800d4a4518e8e87099cb35681524c623488581b
jmiller2696 <jmiller2696@gmail.com>
2023-09-05 15:54:14 -0400

    5

---
+++ assignmentOne.js
@@ -0,0 +1,4 @@
+let ticketNum = 3; ticketCost =14;
+document.getElementById('ticketNum').innerHTML += 3;
+document.getElementById('ticketCost').innerHTML += 14;
+document.getElementById('ttCost').innerHTML += ticketNum * ticketCost;
